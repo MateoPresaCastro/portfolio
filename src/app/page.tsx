59441ef09c5e2c965cf3767541d4b0cf42d7961e
Mateo Presa <mateo.presa.castro@viaplaygroup.com>
2023-11-18 19:29:03 +0100

    add transition

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,15 @@
 export default function Home() {
   return (
     <main className="flex h-screen items-center justify-center bg-neutral-900">
-      <div className="m-10">
+      <div className="animate-fadeIn m-10">
         <div className="max-w-2xl text-neutral-200">
-          <h1 className="mb-2">Mateo Presa Castro</h1>
+          <h1 className="mb-2 delay-100">Mateo Presa Castro</h1>
           <p className="mb-12 font-light">
             Pursuing purpose, truth, and beauty.
           </p>
 
           <h1 className="mb-2">Now</h1>
-          <p className="mb-12 font-light ">
+          <p className="mb-12 font-light">
             {"Backend software engineer @ Viaplay's payments team."}
           </p>
 
