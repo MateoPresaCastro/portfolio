8b95d6abd0403d18dd65eb6c04958d3e06b3f25e
Mateo Presa <mateo.presa.castro@viaplaygroup.com>
2023-12-27 11:27:15 +0100

    trying to get images to work

---
+++ src/app/discography/page.tsx
@@ -0,0 +1,53 @@
+"use client";
+import * as React from "react";
+import Image from "next/image";
+import reezy from "../../../public/reezy.jpg";
+import { Card, CardContent } from "@/components/ui/card";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
+
+export default function Discography() {
+  return (
+    <div className="flex flex-grow items-center justify-center bg-neutral-900">
+      <div className="m-10">
+        <div className="max-w-2xl text-neutral-300">
+          <Carousel
+            opts={{
+              align: "start",
+            }}
+            className="w-full max-w-sm"
+          >
+            <CarouselContent>
+              {Array.from({ length: 5 }).map((_, index) => (
+                <CarouselItem key={index} className="basis-96">
+                  <div className="p-1">
+                    <Card>
+                      <CardContent className="flex aspect-square items-center justify-center p-6">
+                        <span className="text-3xl font-semibold">
+                          {index + 1}
+                        </span>
+                        <Image
+                          src="/reezy.jpg"
+                          height={500}
+                          width={500}
+                          alt="testing"
+                        />
+                      </CardContent>
+                    </Card>
+                  </div>
+                </CarouselItem>
+              ))}
+            </CarouselContent>
+            <CarouselPrevious />
+            <CarouselNext />
+          </Carousel>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className + " flex min-h-screen flex-col"}>
+      <body className={inter.className + " flex min-h-screen flex-col antialiased"}>
         {children}
         <Footer />
       </body>
