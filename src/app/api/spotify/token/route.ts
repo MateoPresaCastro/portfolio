604e31583592a2f27a1b764837f3de3a2e399b36
Mateo Presa <mateo.presa.castro@viaplaygroup.com>
2023-12-28 09:59:29 +0100

    cors on next config file

--- src/app/api/spotify/token/route.ts
+++ src/app/api/spotify/token/route.ts
@@ -2,24 +2,17 @@ import { cache } from "@/lib/utils";
 import { NextRequest } from "next/server";
 export const dynamic = "force-dynamic";
 
-const headers = {
-  "Access-Control-Allow-Origin": "https://www.mateopresacastro.me",
-};
-
 export async function GET(request: NextRequest) {
   const state = request.nextUrl.searchParams.get("state");
   if (!state) {
     console.log("No state");
-    return Response.json({ error: "No state" }, { status: 400, headers });
+    return Response.json({ error: "No state" }, { status: 400 });
   }
 
   const access_token = cache.get(state);
   if (!access_token) {
-    return Response.json(
-      { error: "No access token" },
-      { status: 500, headers },
-    );
+    return Response.json({ error: "No access token" }, { status: 500 });
   }
 
-  return Response.json({ access_token }, { headers });
+  return Response.json({ access_token });
 }
