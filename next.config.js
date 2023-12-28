f4adf53da42a040a74980ef0516df47d9f6a04ac
Mateo Presa <mateo.presa.castro@viaplaygroup.com>
2023-12-28 10:04:00 +0100

    fix unnecessary cors

--- next.config.js
+++ next.config.js
@@ -1,17 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    async headers() {
-        return [
-            {
-                source: "/api/spotify/token",
-                headers: [
-                    { key: "Access-Control-Allow-Credentials", value: "true" },
-                    { key: "Access-Control-Allow-Origin", value: "https://www.mateopresacastro.me" },
-                    { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
-                    { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
-                ]
-            }
-        ]
-    }
-}
-module.exports = nextConfig
+const nextConfig = {};
+
+module.exports = nextConfig;
