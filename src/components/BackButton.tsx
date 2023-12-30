b8b821ab1134e5e424c55ec63c6004132a245d56
Mateo Presa <mateo.presa.castro@viaplaygroup.com>
2023-12-30 23:30:00 +0100

    underline color

--- src/components/BackButton.tsx
+++ src/components/BackButton.tsx
@@ -7,7 +7,7 @@ export default function BackButton() {
     <Link href="/" className="mb-2 ml-1 flex w-16 items-center">
       <ArrowLeftIcon className="text-neutral-500 " />
       <Italic>
-        <p className="ml-1 w-auto cursor-pointer text-neutral-300 underline decoration-neutral-600 decoration-1 underline-offset-[2.5px] transition-all duration-300 hover:decoration-neutral-300">
+        <p className="ml-1 w-auto cursor-pointer text-neutral-300 underline decoration-neutral-500 decoration-1 underline-offset-[2.5px] transition-all duration-300 hover:decoration-neutral-300">
           Back
         </p>
       </Italic>
--- src/components/Connect.tsx
+++ src/components/Connect.tsx
@@ -6,7 +6,7 @@ export default function Connect() {
       <h2>Connect</h2>
       <Link
         href={"mailto:mateopresacastro@gmail.com"}
-        className="mb-16 w-auto cursor-pointer font-light text-neutral-300 underline decoration-neutral-600 decoration-1 underline-offset-[2.5px] transition-all duration-300 hover:decoration-neutral-300"
+        className="mb-16 w-auto cursor-pointer font-light text-neutral-300 underline decoration-neutral-500 decoration-1 underline-offset-[2.5px] transition-all duration-300 hover:decoration-neutral-300"
       >
         mateopresacastro@gmail.com
       </Link>
