538f99238d3504d0eba202020282d2a13c789442
Mateo Presa <mateo.presa.castro@viaplaygroup.com>
2023-12-30 12:10:44 +0100

    fix

--- src/app/credits/page.tsx
+++ src/app/credits/page.tsx
@@ -4,7 +4,7 @@ import BackButton from "@/components/BackButton";
 
 export default function Credits() {
   return (
-    <div className="flex flex-grow items-center justify-center bg-neutral-900">
+    <div className="flex flex-grow items-center justify-center">
         <div className="ml-2 m-10 max-w-2xl">
           <BackButton />
           <MyCarousel />
