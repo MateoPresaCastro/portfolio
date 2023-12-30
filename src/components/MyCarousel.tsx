29cde013710e405d17cae120c9feb72277d56caf
Mateo Presa <mateo.presa.castro@viaplaygroup.com>
2023-12-30 09:19:01 +0100

    fix

--- src/components/MyCarousel.tsx
+++ src/components/MyCarousel.tsx
@@ -40,7 +40,7 @@ export default function MyCarousel() {
                     <Link
                       href={youtubeLink}
                       target="_blank"
-                      className="flex items-center justify-center"
+                      className="flex items-center justify-start"
                     >
                       <p className="w-auto cursor-pointer underline decoration-neutral-600 decoration-1 underline-offset-[2.5px] transition-all duration-300 hover:decoration-neutral-400">
                         {`${artist} - ${song}`}
