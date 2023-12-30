697d7fdc5be19b0e3a964bc691eb3c71cd1ba163
Mateo Presa <mateo.presa.castro@viaplaygroup.com>
2023-12-30 12:25:01 +0100

    change color

--- src/components/BackButton.tsx
+++ src/components/BackButton.tsx
@@ -5,7 +5,7 @@ export default function BackButton() {
   return (
     <Link href="/" className="mb-2 ml-1 flex w-16 items-center">
       <ArrowLeftIcon className="text-neutral-500 " />
-      <p className="ml-1 w-auto cursor-pointer text-neutral-300 underline decoration-neutral-600 decoration-1 underline-offset-[2.5px] transition-all duration-300 hover:decoration-neutral-400">
+      <p className="ml-1 w-auto cursor-pointer text-neutral-500 underline decoration-neutral-600 decoration-1 underline-offset-[2.5px] transition-all duration-300 hover:decoration-neutral-400">
         Back
       </p>
     </Link>
