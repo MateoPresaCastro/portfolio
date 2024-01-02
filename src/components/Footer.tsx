da401074f03921517c15fded9431caf5759820ad
Mateo Presa <mateo.presa.castro@viaplaygroup.com>
2024-01-02 10:22:08 +0100

    change icon size

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,12 +4,15 @@ import { FaGithub, FaLinkedin } from "react-icons/fa";
 export default function Footer() {
   return (
     <footer className="flex h-16 items-center justify-center">
-      <div className="flex w-96 max-w-2xl items-center justify-center font-light text-neutral-500">
-        <Link target="_blank" href="https://github.com/MateoPresaCastro/portfolio">
-          <FaGithub className="mx-6 cursor-pointer text-xl transition-all duration-300 hover:text-neutral-200" />
+      <div className="flex w-96 max-w-2xl items-center justify-center text-lg font-light text-neutral-500">
+        <Link
+          target="_blank"
+          href="https://github.com/MateoPresaCastro/portfolio"
+        >
+          <FaGithub className="mx-6 cursor-pointer transition-all duration-300 hover:text-neutral-200" />
         </Link>
         <Link target="_blank" href="https://www.linkedin.com/in/mateopresa/">
-          <FaLinkedin className="mx-6 cursor-pointer text-xl transition-all duration-300 hover:text-neutral-200" />
+          <FaLinkedin className="mx-6 cursor-pointer transition-all duration-300 hover:text-neutral-200" />
         </Link>
       </div>
     </footer>
